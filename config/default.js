eaa6bd401ede48b2bd01ce1ce596771790711097
Frank Kuipers <frank.k994@gmail.com>
2016-04-26 15:48:45 +0200

    Added module configuration

---
+++ config/default.js
@@ -0,0 +1,9 @@
+module.exports = {
+    production: true,
+    mongodb: {
+        host: 'ds013310.mlab.com:13310',
+        username: 'API',
+        password: 'zW3nGe1P4Ssw0rD',
+        database: 'heroku_sc24nhq2'
+    }
+};
