b6738a92de6cc8bcbf64cb6a3a3b327ac4b91b39
WoutervanGeel <w.vangeel@student.avans.nl>
2016-06-18 23:13:26 +0200

    nicely started on admin page

--- public/scripts/adminpage.js
+++ public/scripts/adminpage.js
@@ -1,10 +1,49 @@
-console.log("test1");
-$('.testClass').append("test");
-console.log("test2");
-$.ajax({
-url: '/races?format=json',
-beforeSend: function(xhr){xhr.setRequestHeader('Auth', 'admin');},
-method: 'get' ,
-success: function successmethod(data) {console.log(data)},
-failure: function failuremethod() {},
-});
+var appl = new Application();
+
+function Application(){
+
+	var self = this;
+    
+    self.addVenue = function(){
+        var name = $('#newVenueName').val();
+        var category = $('#newVenueCategory').val();
+        if(name == ""){
+            alert("name is empty");
+        }else if(category == ""){
+            alert("category is empty");
+        } else {
+            var venue = {
+                name: name,
+                category: category
+            };
+            
+            $.ajax({
+                url: '/venues?format=json',
+                beforeSend: function(xhr){xhr.setRequestHeader('Auth', 'admin');},
+                method: 'post',
+                data: venue,
+                success: function successmethod(data) {console.log(data)},
+                failure: function failuremethod(data) {console.log(data)},
+            });
+        }
+    }
+    
+    self.connectButtons = function(){
+        //$('#editbutton').on('click', self.addVenue);
+        $('#editRaceButton').on('click', function(e){
+            e.preventDefault();
+            //var id = e.target.parentElement.id;
+            // if(id != "load"){
+            //     self.selectedPokemon = self.getNewPokemon(id);
+            //     $.mobile.changePage('#detail-page');
+            // }
+            alert("editing race");
+        });
+        
+        $('#addVenueButton').on('click', function(e){
+           e.preventDefault();
+           self.addVenue(); 
+        });
+    }
+    $(document).ready(self.connectButtons);
+}
